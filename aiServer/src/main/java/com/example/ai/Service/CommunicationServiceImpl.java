7cd7c64381dd31cdbf52684d153ac5064b981d9e
2-jjong <jhyeon2450@gmail.com>
2024-08-02 01:35:49 +0900

    :white_check_mark: eureka 연결 수정

--- aiServer/src/main/java/com/example/ai/Service/CommunicationServiceImpl.java
+++ aiServer/src/main/java/com/example/ai/Service/CommunicationServiceImpl.java
@@ -2,6 +2,7 @@ package com.example.ai.Service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.http.*;
@@ -18,50 +19,52 @@ import java.util.Map;
 @Service
 public class CommunicationServiceImpl implements CommunicationService{
     private final DiscoveryClient discoveryClient;
+    private final RestTemplate restTemplate;
     private final Logger logger = LoggerFactory.getLogger(CommunicationServiceImpl.class);
 
-    public CommunicationServiceImpl(DiscoveryClient discoveryClient) {
+    @Autowired
+    public CommunicationServiceImpl(DiscoveryClient discoveryClient,
+                                    RestTemplate restTemplate) {
         this.discoveryClient = discoveryClient;
+        this.restTemplate = restTemplate;
     }
 
     @Override
     public String imageUpload(byte[] image) throws URISyntaxException {
         try {
-            ServiceInstance imageService = discoveryClient.getInstances("IMAGESERVER").get(0);
-            RestTemplate restTemplate = new RestTemplate();
+            ServiceInstance imageService = discoveryClient.getInstances("IMAGE-SERVER").get(0);
             HttpHeaders headers = new HttpHeaders();
-            HttpEntity<?> http;
+            headers.setContentType(MediaType.APPLICATION_JSON);
 
             Map<String, byte[]> bodyMap = new HashMap<>();
             bodyMap.put("image", image);
-            headers.setContentType(MediaType.APPLICATION_JSON);
-            http = new HttpEntity<>(bodyMap, headers);
-
+            HttpEntity<Map<String, byte[]>> http = new HttpEntity<>(bodyMap, headers);
+            logger.info("imageServer URL : " + imageService.getUri().toString());
             URI uri = new URI(imageService.getUri() + "/image/byteImage");
-            ResponseEntity response = restTemplate.exchange(uri, HttpMethod.POST, http, LinkedHashMap.class);
+            ResponseEntity<LinkedHashMap> response = restTemplate.exchange(uri, HttpMethod.POST, http, LinkedHashMap.class);
 
             if (response.getStatusCode().is2xxSuccessful()) {
                 logger.info("Successfully uploaded image");
-                LinkedHashMap responseBody = (LinkedHashMap) response.getBody();
-                List<String> images = (List) responseBody.get("images");
-                String imageUri = (String) images.get(0);
+                LinkedHashMap responseBody = response.getBody();
+                List<String> images = (List<String>) responseBody.get("images");
+                String imageUri = images.get(0);
                 logger.info("Image URI: {}", imageUri);
 
                 return imageUri;
+            } else {
+                logger.error("Failed to upload image, status code: {}", response.getStatusCode());
+                throw new RuntimeException("Failed to upload image");
             }
-        } catch (HttpClientErrorException e) {
-            logger.error("Failed to upload image");
-            return "Failed to upload image";
+        } catch (Exception e) {
+            logger.error("Failed to upload image", e);
+            throw new RuntimeException("Failed to upload image", e);
         }
-        logger.error("Failed to upload image");
-        return "Failed to upload image";
     }
 
     @Override
     public String imageUpdate(String image, String imageKey) throws URISyntaxException{
         try {
-            ServiceInstance imageService = discoveryClient.getInstances("IMAGE-SERVICE").get(0);
-            RestTemplate restTemplate = new RestTemplate();
+            ServiceInstance imageService = discoveryClient.getInstances("IMAGE-SERVER").get(0);
             HttpHeaders headers = new HttpHeaders();
 
             Map<String, String> bodyMap = new HashMap<>();
@@ -110,8 +113,7 @@ public class CommunicationServiceImpl implements CommunicationService{
     @Override
     public Boolean imageDelete(String imageKey) throws URISyntaxException{
         try {
-            ServiceInstance imageService = discoveryClient.getInstances("IMAGE-SERVICE").get(0);
-            RestTemplate restTemplate = new RestTemplate();
+            ServiceInstance imageService = discoveryClient.getInstances("IMAGE-SERVER").get(0);
             HttpHeaders headers = new HttpHeaders();
             HttpEntity<?> http = new HttpEntity<>(headers);
 
