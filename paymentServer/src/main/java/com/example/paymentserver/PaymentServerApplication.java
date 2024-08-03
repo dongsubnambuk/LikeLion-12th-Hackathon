d5b4aea3b5421060e5c7829dc6e76595a815b45a
2-jjong <jhyeon2450@gmail.com>
2024-08-03 19:38:11 +0900

    :white_check_mark: 서버 시간 변경

--- paymentServer/src/main/java/com/example/paymentserver/PaymentServerApplication.java
+++ paymentServer/src/main/java/com/example/paymentserver/PaymentServerApplication.java
@@ -1,10 +1,17 @@
 package com.example.paymentserver;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.TimeZone;
+
 @SpringBootApplication
 public class PaymentServerApplication {
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(PaymentServerApplication.class, args);
