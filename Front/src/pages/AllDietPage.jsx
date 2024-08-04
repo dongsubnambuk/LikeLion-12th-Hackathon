60cce4712955d605bbfa0fff506ceb57c495bdf1
dongsubnambuk <ehdtjq1278@naver.com>
2024-08-04 17:41:26 +0900

    식단 css 변경

--- Front/src/pages/AllDietPage.jsx
+++ Front/src/pages/AllDietPage.jsx
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { Card, Spin } from 'antd';
 import '../CSS/AllDietPage.css';
 import Header from '../components/Header';
 import BottomNav from '../components/BottomNav';
 import logo from '../images/logo.png';
+
+const { Meta } = Card;
 
 function AllDietPage() {
     const navigate = useNavigate();
@@ -30,7 +33,7 @@ function AllDietPage() {
 
             if (response.status === 200) {
                 setMealData(result.dailyMealPlans);
-                console.log(result)
+                console.log(result);
             } else {
                 console.log("실패");
                 alert("실패: " + result.message);
@@ -39,15 +42,13 @@ function AllDietPage() {
         handleGet();
     }, []);
 
-    // 데이터가 로딩 중일 때 보여줄 화면
     if (mealData.length === 0) {
-        return <div>Loading...</div>;
+        return <Spin size="large" tip="Loading..." />;
     }
 
-    // mealData에서 원하는 형식으로 item 배열을 생성
     const items = mealData.flatMap(day =>
         day.mealOptions.flatMap(option =>
-            option.foodMenus.slice(0, 1) // foodMenus의 첫 번째 항목만 사용
+            option.foodMenus.slice(0, 1)
         )
     );
 
@@ -58,13 +59,17 @@ function AllDietPage() {
             <div className="ADPcontainer">
                 <div className="ADPlistContainer">
                     {items.map((item, index) => (
-                        <div key={index} className="ADPitemCard" onClick={() => handleItemClick(item)}>
-                            <div className="ADPitemTitle">{item.name}</div>
-                            <div className="ADPitemImage">
-                                <img src={logo} style={{ width: '100%', height: '100%' }} alt="logo" />
+                        <Card
+                            key={index}
+                            hoverable
+                            className="ADPitemCard"
+                            onClick={() => handleItemClick(item)}
+                        >
+                            <div className="ADPitemImageContainer">
+                                <img src={logo} alt="logo" className="ADPitemImage" />
                             </div>
-                            <div className="ADPitemPrice">{item.price}</div>
-                        </div>
+                            <Meta title={<div className="ADPitemTitle">{item.name}</div>} description={`가격: ${item.price}`} className="ADPitemMeta" />
+                        </Card>
                     ))}
                 </div>
             </div>
