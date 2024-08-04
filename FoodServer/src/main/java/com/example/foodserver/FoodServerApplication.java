9211f2fb59666f2bc85e9f159d25ae2843da46df
2-jjong <jhyeon2450@gmail.com>
2024-08-04 19:20:06 +0900

    :white_check_mark: 서버 시간 추가

--- FoodServer/src/main/java/com/example/foodserver/FoodServerApplication.java
+++ FoodServer/src/main/java/com/example/foodserver/FoodServerApplication.java
@@ -1,13 +1,20 @@
 package com.example.foodserver;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+
+import java.util.TimeZone;
 
 
 @SpringBootApplication
 @EntityScan(basePackages = "com.example.foodserver.Entity")
 public class FoodServerApplication {
+    @PostConstruct
+    void started(){
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
+    }
     public static void main(String[] args) {
         SpringApplication.run(FoodServerApplication.class, args);
     }
