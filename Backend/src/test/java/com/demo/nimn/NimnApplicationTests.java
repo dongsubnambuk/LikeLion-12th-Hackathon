c325a8a1ce48a7b3535e7099e578f7c1d2f8615f
jonghyeon <jhyeon2450@gmail.com>
2025-06-05 23:31:24 +0900

    add backend

---
+++ Backend/src/test/java/com/demo/nimn/NimnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.nimn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
