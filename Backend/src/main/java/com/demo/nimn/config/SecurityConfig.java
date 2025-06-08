d2a2064a5000fd8787b39833f240cc930fb977c4
ryusemin <tpals3799@naver.com>
2025-06-09 01:27:28 +0900

    feat: 이메일 인증 구현 및 테스트 (#48 )

--- Backend/src/main/java/com/demo/nimn/config/SecurityConfig.java
+++ Backend/src/main/java/com/demo/nimn/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
         //경로별 인가 작업
         http
                 .authorizeHttpRequests((auth) -> auth
-                        .requestMatchers("/users/login","/", "/users/signup").permitAll() // 해당 경로에 대해 모든 사용자가 접근할 수 있도록 허용. 인증되지 않은 사용자도 접근가능
+                        .requestMatchers("/users/login","/users/isExist", "/users/signup", "/email/**").permitAll() // 해당 경로에 대해 모든 사용자가 접근할 수 있도록 허용. 인증되지 않은 사용자도 접근가능
                         .requestMatchers("/users/hello").hasRole("ADMIN") // ROLE_ADMIN 권한을 가진 사용자만 접근할 수 있도록 설정
                         .anyRequest().authenticated()); // 그 외의 모든 경로는 인증된 사용자라면 접근 가능
 
