7caa76841045b6750be330da3ef67e5b91a49e09
donggyun-han <hdg5639@daum.net>
2025-06-09 22:17:38 +0900

    refactor: 불필요한 notification 레포지토리 파일 삭제

--- Backend/src/main/java/com/demo/nimn/repository/notification/SurveyNotificationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.demo.nimn.repository.notification;
-
-import com.example.notificationserver.Entity.SurveyNotificationEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SurveyNotificationRepository extends JpaRepository<SurveyNotificationEntity, Long> {
-}
