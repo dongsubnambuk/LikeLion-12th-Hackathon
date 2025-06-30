bc1c5bdba97df204ee7c572de23149ee6f31e5a9
ryusemin <tpals3799@naver.com>
2025-07-01 00:00:19 +0900

    feat: 비밀번호 변경 기능 추가 및 유저 정보 수정 로직 수정

--- Backend/src/main/java/com/demo/nimn/service/auth/UserService.java
+++ Backend/src/main/java/com/demo/nimn/service/auth/UserService.java
@@ -1,5 +1,7 @@
 package com.demo.nimn.service.auth;
 
+import com.demo.nimn.dto.auth.PasswordChangeDTO;
+import com.demo.nimn.dto.auth.UserDTO;
 import com.demo.nimn.dto.auth.UserDetails;
 import com.demo.nimn.dto.auth.UsersEmailDTO;
 import jakarta.servlet.http.HttpServletRequest;
@@ -16,6 +18,8 @@ public interface UserService {
     public UserDetails getUserDetail(String email);
 
     public UserDetails updateUser(UserDetails user);
+
+    public UserDTO changePassword(PasswordChangeDTO passwordChangeDto);
 
     public UsersEmailDTO getAllUsersEmail();
 
